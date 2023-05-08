3a21c1ae592bad3061ce62d3ede7449be1928722
Swed_home <supersega@mail.ru>
2023-05-08 23:11:35 +0500

    del 111 and change geometry

--- geometry/views.py
+++ geometry/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def get_rectangle_area(request, width: int, length: int):
+    return HttpResponse(f'Площадь прямоугольника размером {width}x{length} равна {width * length}')
+
+
+def get_square_area(request, width: int):
+    return HttpResponse(f'Площадь квадрата размером {width}x{width} равна {width ** 2}')
+
+
+def get_circle_area(request, radius: int):
+    return HttpResponse(f'Площадь круга радиуса {radius} равна {3.14 * radius ** 2}')
